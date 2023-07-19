884d748a14b82b956715f68eefc7931181ab3a7e
uchenick55 <uchenick55@yandex.ru>
2023-07-19 07:15:32 +0300

    добавили прогресс бар

--- assets/hooks/useLoader.ts
+++ assets/hooks/useLoader.ts
@@ -1,21 +1,24 @@
-import {useRouter} from 'next/router';
-import NProgress from 'nprogress';
-import {useEffect} from 'react';
+import {useRouter} from "next/router";
+import NProgress from "nprogress";
+import {useEffect} from "react";
 
-export const useLoader = () => {
-    const router = useRouter()
 
-    useEffect(() => {
-        const startLoading = () => NProgress.start()
-        const endLoading = () => NProgress.done()
+export const useLoader = () => { // кастомный хук отображения прогресс бара
+    const router = useRouter() // доступ к объекту роутера в Next
+
+    useEffect(()=> {
+        const startLoading = () => NProgress.start() // функция отобразить прогресс бар
+        const endLoading = () => NProgress.done()// функция убрать прогресс бар
 
         router.events.on('routeChangeStart', startLoading)
         router.events.on('routeChangeComplete', endLoading)
         router.events.on('routeChangeError', endLoading)
+
         return () => {
             router.events.off('routeChangeStart', startLoading)
             router.events.off('routeChangeComplete', endLoading)
             router.events.off('routeChangeError', endLoading)
         }
-    }, [router])
+    },[router])
+
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import {ReactElement, ReactNode, useState} from 'react';
 import {NextPage} from 'next';
 import {QueryClient} from '@tanstack/query-core';
 import {Hydrate, QueryClientProvider} from '@tanstack/react-query';
-import {useLoader} from 'assets/hooks/useLoader';
+import {useLoader_old} from '../assets/hooks/useLoader_old';
+import {useLoader} from "../assets/hooks/useLoader";
+//import 'styles/nprogress.css'
 import 'styles/nprogress.css'
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
@@ -19,6 +21,7 @@ export default function App({Component, pageProps}: AppPropsWithLayout) {
     const [queryClient] = useState(() => new QueryClient)
 
     useLoader()
+   // useLoader_old()
 
     const getLayout = Component.getLayout ?? ((page) => page);
 
